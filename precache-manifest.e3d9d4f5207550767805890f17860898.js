1989db028157910f7363a9acee5799b5c7d2de24
Jason Chong <34180725+meljason@users.noreply.github.com>
2020-06-15 17:28:44 -0400

    Updates

---
+++ precache-manifest.e3d9d4f5207550767805890f17860898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38001890bfef66ecaaabb91129e70fc5",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "a4782fd2d0fb6ec3469e",
+    "url": "/todolist-react/static/css/main.8bfbd428.chunk.css"
+  },
+  {
+    "revision": "751db9b5208b48dae19d",
+    "url": "/todolist-react/static/js/2.cc395645.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist-react/static/js/2.cc395645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4782fd2d0fb6ec3469e",
+    "url": "/todolist-react/static/js/main.c7ea0c5b.chunk.js"
+  },
+  {
+    "revision": "f9dbdb07507eb5c20f44",
+    "url": "/todolist-react/static/js/runtime-main.4fc41e77.js"
+  }
+]);
